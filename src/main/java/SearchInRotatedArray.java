06c2b34bb730d6ea0a144e1558f3edb8385c8fc3
Ayodeji Bankole <Ayodeji.Bankole@Interswitchng.com>
2024-05-01 23:42:57 +0100

    .

--- src/main/java/SearchInRotatedArray.java
+++ src/main/java/SearchInRotatedArray.java
@@ -58,4 +58,43 @@ public class SearchInRotatedArray {
         return - 1;
     }
 
+    static int findPivotWithDuplicate(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+//             case 1
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+//           case 2
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                if(arr[start] > arr[start + 1]){
+                    return start;
+                }
+                start++;
+
+                if(arr[end] < arr[end - 1]){
+                    return end - 1;
+                }
+                end--;
+            }
+            else if(arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            }else {
+                end = mid - 1;
+            }
+
+
+        }
+
+        return - 1;
+    }
+
 }
